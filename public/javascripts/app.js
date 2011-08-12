2e1f09cede526b8fd25d69b2b6f23845c48450b8
brousalis <brousapg@gmail.com>
2011-08-12 00:19:01 -0500

    Working json for results

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   $("body").css("display", "none");
   $("body").fadeIn(2000);
   $("button").button();
-  $("form input").autocomplete({
+  $("input").autocomplete({
     source: function(request, response) {
       $.ajax({
         url: '/search/' + $("form input").val(),
@@ -28,14 +28,33 @@ $(document).ready(function(){
         marginTop: "60px"          
       }, 700);      
       $(".results").delay(700).fadeIn(1000);
-      $.ajax({
-         url: '/repo/' + ui.item.owner + '/' + ui.item.name,
-         success: function(data) {
-          $('.results').html(data.ratio);
-         }
-      });
+      call_results(ui);
     }
+  });
+  $('input').keypress(function(e){
+    if(e.which == 13){
+      e.preventDefault();
+      return false;
+    }
+  });
+
+  function call_results(ui) {
+    $.ajax({
+     url: '/repo/' + ui.item.owner + '/' + ui.item.name,
+     success: function(data) {
+      $(".results h1").html(ui.item.name);
+      $(".results h2").html("by <strong>"+ui.item.owner+"</strong>");
+     }
+    }); 
+  }
+  
+  $('#back').click(function(e){
+    $(".results").fadeOut(1000);
+    $(".panel").delay(700).animate({ marginTop:"140px"}, 700);
+    $("input").delay(700).val("").focus();
   });
 });
 
 ;(function(a){a.preload=function(){var c=[],b=arguments.length;for(;b--;){c.push(a("<img />").attr("src",arguments[b]));}};})(jQuery);
+
+
